c9c47b044cd9c1041b1a853a1b38722a73b30e25
aaltoo <scotlandfjord@protonmail.com>
2022-08-02 01:00:54 +0900

    [11] Add my own solution

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,5 +1,8 @@
-let isPlaying = false;
+let isPlaying = false
+let isScrolling = false
 const video = document.querySelector('.viewer')
+const progressFilled = document.querySelector('.progress__filled')
+const progress = document.querySelector('.progress')
 
 function handleButtonClick() {
   if (isPlaying) {
@@ -10,6 +13,14 @@ function handleButtonClick() {
   isPlaying = !isPlaying
 }
 
+function goBack() {
+  video.currentTime -= 10
+}
+
+function goForward() {
+  video.currentTime += 25
+}
+
 document.querySelector('input[name=volume]').addEventListener('input', (e) => {
   video.volume = e.currentTarget.value
 })
@@ -18,3 +29,26 @@ document.querySelector('input[name=playbackRate]').addEventListener('input', (e)
   video.playbackRate = e.currentTarget.value
 })
 
+video.addEventListener('timeupdate', (e) => {
+  const percent = video.duration / 100
+  console.log(e.currentTarget.currentTime / percent)
+  progressFilled.style.width = `${e.currentTarget.currentTime / percent}%`
+})
+
+progress.addEventListener('mousedown', (e) => {
+  const percent = video.duration / 100
+  video.currentTime = e.layerX / e.currentTarget.clientWidth * 100 * percent
+  isScrolling = true
+})
+
+progress.addEventListener('mouseup', (e) => {
+  isScrolling = false
+})
+
+progress.addEventListener('mousemove', (e) => {
+  const percent = video.duration / 100
+  if (isScrolling) {
+    video.currentTime = e.layerX / e.currentTarget.clientWidth * 100 * percent
+    progressFilled.style.width = `${e.layerX}px`
+  }
+})
